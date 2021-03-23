123b0540c3f7d9acae8d0ff1257b0ba690ef6ad5
gbringard <gbringard@ltu.edu>
2021-03-23 16:17:41 -0400

    Adding Middlewares

--- src/server.js
+++ src/server.js
@@ -4,10 +4,24 @@ NodeJS Homework 3
  */
 
 const Codes = require('http-status-codes');
+const Winston = require('winston');
+const expressWinston = require('express-winston');
 const Express = require('express');
 const BodyParser = require('body-parser');
 
 const app = Express();
+
+const winstonLogger = Winston.createLogger({
+  transports: [
+    new Winston.transports.Console({
+      format: Winston.format.simple(),
+    }),
+  ],
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * (10 - 1 + 1) + 1);
+}
 
 app.use(BodyParser.urlencoded({ extended: true }));
 app.use(BodyParser.json());
@@ -20,12 +34,26 @@ app.use((req, res, next) => {
   }
 });
 
-app.all('/', (req, res) => {
-  res.send('Hello World');
+app.use((req, res, next) => {
+  winstonLogger.info(req.method);
+  winstonLogger.info(req.originalUrl);
+  winstonLogger.info(req.body);
+  winstonLogger.info(req.params.toString());
+  winstonLogger.info(req.headers.toString());
+  winstonLogger.info(req.dateValidation);
+  next();
 });
 
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  res.status(Codes.StatusCodes.INTERNAL_SERVER_ERROR).send(err.message);
+app.all('/', (req, res) => {
+  if (getRandomNumber() % 2 === 0) {
+    res.status(Codes.StatusCodes.OK).send('Hello World');
+  } else {
+    throw Error('Oops');
+  }
 });
+
+app.use((err, req, res, next) => {
+  res.status(Codes.StatusCodes.INTERNAL_SERVER_ERROR).send(`We're sorry, the error was: ${err.message}`);
+});
+
 app.listen(8080);
