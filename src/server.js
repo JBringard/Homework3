414ca9fb0855c1afcc4b4a4cc8ea5aed6e0006ed
gbringard <gbringard@ltu.edu>
2021-03-22 23:21:57 -0400

    Added first 2 Middlewares

--- src/server.js
+++ src/server.js
@@ -2,12 +2,30 @@
 Gerard Bringard
 NodeJS Homework 3
  */
-const express = require('express');
 
-const app = express();
+const Codes = require('http-status-codes');
+const Express = require('express');
+const BodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
+const app = Express();
+
+app.use(BodyParser.urlencoded({ extended: true }));
+app.use(BodyParser.json());
+
+app.use((req, res, next) => {
+  if (req.method === 'DELETE') {
+    res.status(Codes.StatusCodes.METHOD_NOT_ALLOWED).send(Codes.ReasonPhrases.METHOD_NOT_ALLOWED);
+  } else {
+    next();
+  }
+});
+
+app.all('/', (req, res) => {
   res.send('Hello World');
 });
 
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(Codes.StatusCodes.INTERNAL_SERVER_ERROR).send(err.message);
+});
 app.listen(8080);
