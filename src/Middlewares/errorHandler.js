fd506a3c38aaaa8dbac00838ae002463787e0882
gbringard <gbringard@ltu.edu>
2021-03-31 18:33:35 -0400

    Fixing no-unused-vars

--- src/Middlewares/errorHandler.js
+++ src/Middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 const Codes = require('http-status-codes');
 
-module.exports = (err, req, res, next) => {
+module.exports = (err, req, res, _next) => {
   res.status(Codes.StatusCodes.INTERNAL_SERVER_ERROR).send(`We're sorry, the error was: ${err.message}`);
 };
